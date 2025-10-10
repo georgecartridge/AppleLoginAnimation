398f5cdb06e6574e62e5c315c0409b4016327d52
George Cartridge <35803468+georgecartridge@users.noreply.github.com>
2025-10-10 03:53:37 +0100

    Initial Commit

---
+++ AppleLoginAnimation/AppleLoginAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleLoginAnimationApp.swift
+//  AppleLoginAnimation
+//
+//  Created by George on 10/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleLoginAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
